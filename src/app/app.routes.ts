5fac4603f33a7b6561634e05372b772ddc7cc773
= <=>
2024-01-18 15:45:33 +0700

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { DefaultComponent } from './layout/default/default.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { PegawaiComponent } from './page/pegawai/pegawai.component';
+import { FormComponent as PegawaiFormComponent } from './page/pegawai/form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: DefaultComponent, children: [
+    {path: 'dashboard', component: DashboardComponent},
+
+    {path: 'pegawai', component: PegawaiComponent},
+    {path: 'pegawai/form', component: PegawaiFormComponent},
+  ]},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NotFoundComponent},
+];
---
+++ src/app/page/pegawai/pegawai.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pegawai',
+  standalone: true,
+  imports: [],
+  templateUrl: './pegawai.component.html',
+  styleUrl: './pegawai.component.css'
+})
+export class PegawaiComponent {
+
+}

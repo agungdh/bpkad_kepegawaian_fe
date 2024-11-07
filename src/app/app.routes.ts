94c39d35e6d14ca54e1225860c8f49e9b0c2cce9
AgungDH <agungdh@live.com>
2024-11-07 10:28:38 +0700

    init flowbite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './layout/default/default.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'dashboard', component: DefaultComponent}
+];
